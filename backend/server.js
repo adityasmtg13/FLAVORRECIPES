168b63d2d5fc9afb9f8055c4c0e9a180f5a35f8e
adityasmtg13 <aditya.23bce8387@vitapstudent.ac.in>
2026-02-28 09:50:43 +0530

    backend ready for render

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
